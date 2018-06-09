17b76cb3e48db72ff5728aaa9103c678ace5e777
John Tromp <john.tromp@gmail.com>
2018-06-09 14:16:30 +0200

    fix 192,7 cuda support

--- equi_miner.cu
+++ equi_miner.cu
@@ -10,8 +10,6 @@
 typedef uint16_t u16;
 typedef uint64_t u64;
 
-#define htole32(x) (x)
-
 #define checkCudaErrors(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true) {
   if (code != cudaSuccess) {
